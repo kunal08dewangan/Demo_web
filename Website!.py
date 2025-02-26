1106cd09dc0ec890b02e298d3ccea53dd073839d
kunal08dewangan <konaldewangan08@gmail.com>
2025-02-26 15:40:10 +0530

    Update Website!.py

--- Website!.py
+++ Website!.py
@@ -3,12 +3,17 @@ import streamlit as st
 st.title("Heyy!!")
 st.subheader("I'm Kunal Dewangan !")
 st.markdown("I'm Just A Developer...")
-\n
+st.set_page_config(
+    page_icon = "M.png",
+    page_title = "KD 1st"
+)
 st.title("Enter Your Personal Details :-")
 name = st.text_input("Enter your Name : ")
 age = st.text_input("Enter Your Age : ")
+univer = st.text_input("Enter Your University Name : ")
 button = st.button("Enter")
 if button:
     st.markdown(f"""
     Name : {name}\n
-    Age : {age}""")
+    Age : {age}
+    University : {univer} """)
