f53acdadbc3dcb1b397aea48a2a6e68323940233
kunal08dewangan <konaldewangan08@gmail.com>
2025-02-26 14:39:55 +0530

    Update Website!.py

--- Website!.py
+++ Website!.py
@@ -3,6 +3,8 @@ import streamlit as st
 st.title("Heyy!!")
 st.subheader("I'm Kunal Dewangan !")
 st.markdown("I'm Just A Developer...")
+\n
+st.title("Enter Your Personal Details :-")
 name = st.text_input("Enter your Name : ")
 age = st.text_input("Enter Your Age : ")
 button = st.button("Enter")
